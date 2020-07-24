e17c04067c4f3e242715d87cfcff297f763ea60a
Ernest Bystrzycki <Ernestbystrzycki@gmail.com>
2020-07-24 19:53:28 +0200

    Init commit

---
+++ Keylogger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+
+
+namespace Keylogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    
+    
+    
+    
+    }
+}
